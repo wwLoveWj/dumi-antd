632999c750fb26c8ba1a84eccf54a6911af054c1
wwLoveWj <blww885@163.com>
2024-09-27 19:10:12 +0800

    feat(更新通知msg): msg
    msg通知

--- scripts/notice.js
+++ scripts/notice.js
@@ -1,9 +1,9 @@
 // 通知发布notify
 const notifier = require('node-notifier'); //在 Node.js 中发送跨平台通知的工具
-// const msg = `${process.env.npm_package_name}@${process.env.npm_package_version} 发布成功`;
+const message = `${process.env.npm_package_name}@${process.env.npm_package_version} 发布成功`;
 notifier.notify({
   title: '发布成功~',
-  message: '项目发布成功了，哈哈哈~',
+  message,
   sound: 'Submarine',
   closeLabel: 'CANCEL',
   actions: 'OK',
