7a010a128686717c665005e713e9d567c9abda03
wwLoveWj <blww885@163.com>
2024-10-13 14:35:23 +0800

    feat: 兼容横向纵向模块变量

--- src/components/WjRadio/index.md
+++ src/components/WjRadio/index.md
@@ -14,6 +14,8 @@ cover: /images/wjcopy.png
 
 | 参数     | 说明               | 类型        | 默认值 |
 | -------- | ------------------ | ----------- | ------ |
-| options     | 以配置形式设置子元素     | `string[] \| number[] \| Array<CheckboxOptionType>`    |   -     |
+| options     | 以配置形式设置子元素     | `Array<RadioOptionType>`    |   -     |
 | value     | 用于设置当前选中的值     | `any`    |   -     |
 | onChange | 选项变化时的回调函数 | `function(e:Event)` |   -     |
+| labelWidth | label的宽度 | `string \| number` |   200    |
+| styles | radio的相关步长配置 | ` Record<string, string>` |   `{ '--colorRadio': '#255ff4','--tranlateX': '6.65em','--tranlateY':'-2.5em',}`    |
--- src/components/WjRadio/index.tsx
+++ src/components/WjRadio/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './style.scss';
+
+export interface RadioOptionType {
+  label: string;
+  value: string;
+}
 export default function WjRadio({
   options = [
     {
@@ -20,63 +25,73 @@ export default function WjRadio({
    */
   value,
   onChange,
+  labelWidth = 200,
+  styles = {
+    '--colorRadio': '#255ff4',
+    '--tranlateX': '6.65em',
+    '--tranlateY': '-2.5em',
+  },
 }: {
-  options?: { label: string; value: string }[];
+  options?: RadioOptionType[];
   value: string;
   onChange: (value: string) => void;
+  styles?: Record<string, string>;
+  labelWidth?: string | number;
 }) {
   return (
-    <form>
-      {options?.map((item) => (
-        <>
-          <input
-            type="radio"
-            id={item.value}
-            name="hopping"
-            value={item.value}
-            onChange={(e) => {
-              onChange(e.target.value); // 当改变时，传递新的值给父组件
-            }}
-            checked={item.value === value}
-          />
-          <label htmlFor={item.value}>
-            <span></span>
-            {item.label}
-          </label>
-        </>
-      ))}
-      <div className="worm">
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-        <div className="worm__segment"></div>
-      </div>
-    </form>
+    <div className="wj-radio" style={styles}>
+      <form>
+        {options?.map((item) => (
+          <>
+            <input
+              type="radio"
+              id={item.value}
+              name="hopping"
+              value={item.value}
+              onChange={(e) => {
+                onChange(e.target.value); // 当改变时，传递新的值给父组件
+              }}
+              checked={item.value === value}
+            />
+            <label htmlFor={item.value} style={{ width: labelWidth }}>
+              <span></span>
+              {item.label}
+            </label>
+          </>
+        ))}
+        <div className="worm">
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+          <div className="worm__segment"></div>
+        </div>
+      </form>
+    </div>
   );
 }
